5c5ebac8ea6c30310397c05582fec9f5bee4b22f
cpore <caseypore@gmail.com>
2014-02-23 20:01:39 -0700

    Should be finished. Needs more test cases.

--- CupTestCases/regress.sh
+++ CupTestCases/regress.sh
@@ -7,16 +7,26 @@ for filename in `ls *.java`
 do
     echo "Regression testing MJPA2.jar $filename"
 
+    # Make AVR assembly from our test program using our compiler
     java -jar ../MJ.jar $filename
-    java -jar MJSIM.jar -b -f $filename.s > t1
+    # Don't run tests if our code doesn't compile
+    if [ $? -eq 0 ]
+    then
+      # Run our assembly code in the Meggy Simulator without the UI
+      java -jar MJSIM.jar -b -f $filename.s > t1
 
-    javac $filename
-    noext=`echo $filename| cut -d'.' -f 1`
-    java $noext > t2
-    diff t1 t2 
+      #Compile our test program with java
+      javac $filename
+      noext=`echo $filename| cut -d'.' -f 1`
+      # Run the java-compiled program
+      java $noext > t2
+      diff t1 t2
+    else
+      echo "Skipping Tests."
+    fi
+
     echo "DONE with $filename"
     echo "============================="
-
 done
 
 # I added this to clean up the files afterwards -cpore
