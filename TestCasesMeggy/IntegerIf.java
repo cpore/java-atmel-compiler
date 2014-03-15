256a2d57da047d46d1edd455485e0bca024bdceb
cpore <caseypore@gmail.com>
2014-03-15 02:53:45 -0600

    Need to fix if minus problem

--- TestCasesMeggy/IntegerIf.java
+++ TestCasesMeggy/IntegerIf.java
@@ -7,8 +7,12 @@ import meggy.Meggy;
 
 class IntegerIf {
     public static void main(String[] whatever){
-	if (Meggy.Color.BLUE == Meggy.Color.BLUE){
-	    Meggy.setPixel( (byte)(1+6), (byte)(5), Meggy.Color.GREEN );
+	if ((byte)1 == (byte)1){
+	    Meggy.setPixel( (byte)(1+ (byte)5), (byte)(5-1), Meggy.Color.GREEN );
+    }
+
+    if ((5+1) == 6){
+	    Meggy.setPixel( (byte)(1+5), (byte)(5), Meggy.Color.GREEN );
     }
 	}
 }
