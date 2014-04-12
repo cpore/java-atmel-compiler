a73a84dad441455bc9c43d8f909912caaf447fe8
cpore <caseypore@gmail.com>
2014-04-11 19:36:55 -0600

    PA4 is done.

--- TestTypeCheck/PA4Tests/created/call.java
+++ TestTypeCheck/PA4Tests/created/call.java
@@ -2,6 +2,7 @@ import meggy.Meggy;
 
 /*
 * Test for various things when calling a fucntion
+* Uncomment the code to test
 *
 */
 
@@ -14,27 +15,40 @@ class call {
         //new A().setZ(5);
 
         // call method with wrong number of arguments
-        new B().setB(Meggy.Color.BLUE, 3, 4);
+        //new B().setB(Meggy.Color.BLUE, 3);
+
+        //Test CallExp
+        Meggy.setPixel(new A().getByte(5), (byte)2, new B().getBlue());
  
     }
 }
 
 class A {
-   public void setA(int y) {
+   public void setA(Meggy.Color y) {
         Meggy.setPixel((byte)5, (byte)2, y);
+    }
+
+    public byte getByte(int i){
+        return (byte)i;
     }
 
     
 }
 class B {
    public void setB(int y, int x, Meggy.Color z) {
-        Meggy.setPixel((byte)5, (byte)2, y);
+        Meggy.setPixel((byte)5, (byte)2, z);
+    }
+
+    public Meggy.Color getBlue(){
+        return Meggy.Color.BLUE;   
     }
 
 }
 
 class C {
-   public void setC(int y) {
+   public void setC(Meggy.Color y) {
+        // call class from another class
+        new B().setB(3, 4, Meggy.Color.BLUE);
         Meggy.setPixel((byte)5, (byte)2, y);
     }
 
